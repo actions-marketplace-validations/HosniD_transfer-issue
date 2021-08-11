b7789507f14b0fd0d28089cb0e5082e4cfb9c8ed
Hosni Dahmani <hosni.dahmani@outlook.com>
2021-08-11 12:11:34 +0200

    - first release automating issues

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const Core = require('@actions/core');
+const { Octokit }  = require("@octokit/rest")
+const Github = require("@actions/github")
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    Core.startGroup("🚦 Checking Inputs and Initializing...")
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const repoSource = Core.getInput('repo-source')
+    const repoDestination = Core.getInput('repo-destination')
 
-    core.setOutput('time', new Date().toTimeString());
+    Core.info("Auth with GitHub Token...")
+    const octokit = new Octokit()
+    Core.info("Done.")
+    Core.endGroup()
+
+    Core.startGroup("📑 Getting all Issues in repository...")
+    let page = 1
+    const issuesData = []
+    let issuesPage
+    do {
+      Core.info(`Getting data from Issues page ${page}...`)
+      issuesPage = await octokit.issues.listForRepo({
+        repo: repoSource,
+        state: "all",
+        page
+      });
+      console.log('issuesPage', issuesPage)
+      Core.info(`There are ${issuesPage.data.length} Issues...`)
+      issuesData.push(issuesPage.data)
+      if (issuesPage.data.length) {
+        Core.info("Next page...")
+      }
+      page++
+    } while (issuesPage.data.length)
+    console.log('issuesData', issuesData)
+
+    // Core.setOutput('time', new Date().toTimeString());
   } catch (error) {
-    core.setFailed(error.message);
+    Core.setFailed(error.message);
   }
 }
 
